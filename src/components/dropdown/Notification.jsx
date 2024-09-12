0fbbdff2215d67cad4533e0caa1000e635c52250
khj <ghghjj652@naver.com>
2024-09-12 19:33:00 +0900

    feat: 알림창 초기 구현

---
+++ src/components/dropdown/Notification.jsx
@@ -0,0 +1,8 @@
+import style from "../css/Notification.module.css";
+const Notification = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.review}></div>
+    </div>
+  );
+};
