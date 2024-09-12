55e4d2029734a286c6566ba1ec7025626a947b79
khj <ghghjj652@naver.com>
2024-09-12 16:26:19 +0900

    feat: main페이지 구현

---
+++ src/components/HeadBox.jsx
@@ -0,0 +1,10 @@
+import style from "./css/HeadBox.module.css";
+const HeadBox = () => {
+  return (
+    <div className={style.headContainer}>
+      <div className={style.searchWrap}></div>
+    </div>
+  );
+};
+
+export default HeadBox;
