fe7f364cc7fe4f9bac9083e28136eb6ab7ad47bd
khj <ghghjj652@naver.com>
2024-09-13 22:32:29 +0900

    feat: 유저 검색 모달창 초기 구현

--- src/components/modal/SearchUser.jsx
+++ src/components/modal/SearchUser.jsx
@@ -1,26 +1,26 @@
-// import style from "../css/SearchUser.module.css";
-// const SearchUser = (props) => {
-//   return (
-//     <div
-//       className={style.modalContainer}
-//       ref={props.modalBackground}
-//       onClick={(e) => {
-//         if (e.target === modalBackground.current) {
-//           props.setModalOpen(false);
-//         }
-//       }}
-//     >
-//       <div className={style.modalContent}>
-//         <p>리액트로 모달 구현하기</p>
-//         <button
-//           className={style.modalCloseBtn}
-//           onClick={() => props.setModalOpen(false)}
-//         >
-//           모달 닫기
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
+import React, { useState } from "react";
+import style from "../css/SearchUser.module.css";
+import { FaPaperclip, FaImage, FaCode } from "react-icons/fa";
+import searchicon from "../img/searchicon.svg";
 
-// export default SearchUser;
+const SearchUser = ({ isOpen, closeModal }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={style.modal}>
+      <div className={style.modalContent}>
+        <div className={style.modalHeader}>사용자 검색</div>
+        <div>
+          <input
+            type="text"
+            placeholder="유저를 검색해보세요"
+            className={style.searchInput}
+          />
+          <img className={style.searchicon} src={searchicon} alt="검색" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchUser;
