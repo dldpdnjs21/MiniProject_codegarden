a487029eb2c8e96cfd7adfca6360ce8c3ab4f9de
khj <ghghjj652@naver.com>
2024-09-20 10:38:53 +0900

    fix: 마이페이지 화면 디자인 수정

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -63,38 +63,42 @@ const EditProfile = () => {
     <div className={style.container}>
       <img src={profileImg} className={style.profileImg} />
       <div className={style.form}>
-        <div className={style.labels}>
+        {/* <div className={style.labels}>
           <div className={style.label}>닉네임</div>
           <div className={style.label}>개발분야</div>
           <div className={style.label}>기술스택</div>
           <div className={style.label}>내 소개</div>
-        </div>
+        </div> */}
         <div className={style.inputs}>
           <div className={style.inputArea}>
-            {/* <div className={style.label}>닉네임</div> */}
+            <div className={style.label}>닉네임</div>
             <input
               type="text"
               value={formData.nickname}
               onChange={handleChange}
+              className={style.inputBox}
             />
           </div>
           <div className={style.inputArea}>
-            {/* <div className={style.label}>개발분야</div> */}
-            <select
-              name="developmentField"
-              //   value={formData.developmentField}
-              onChange={handleChange}
-              defaultValue={formData.developmentField}
-              required
-            >
-              <option value="">개발 분야</option>
-              <option value="frontend">프론트엔드</option>
-              <option value="backend">백엔드</option>
-              <option value="fullstack">풀스택</option>
-            </select>
+            <div className={style.label}>개발분야</div>
+            <div className={style.selectBox}>
+              <select
+                name="developmentField"
+                //   value={formData.developmentField}
+                onChange={handleChange}
+                defaultValue={formData.developmentField}
+                className={style.textarea}
+                required
+              >
+                <option value="">개발 분야</option>
+                <option value="frontend">프론트엔드</option>
+                <option value="backend">백엔드</option>
+                <option value="fullstack">풀스택</option>
+              </select>
+            </div>
           </div>
           <div className={style.inputArea}>
-            {/* <div className={style.label}>기술스택</div> */}
+            <div className={style.label}>기술스택</div>
             {techStackData.map((item) => (
               <div>
                 <input
@@ -109,11 +113,11 @@ const EditProfile = () => {
             ))}
           </div>
           <div className={style.inputArea}>
-            {/* <div className={style.label}>내 소개</div> */}
-            <input
-              type="text"
+            <div className={style.label}>내 소개</div>
+            <textarea
               value={formData.introduce}
               onChange={handleChange}
+              className={style.textBox}
             />
           </div>
         </div>
