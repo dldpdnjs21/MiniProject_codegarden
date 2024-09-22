5438df2edadfa9c34c0c307e0c94c5178ce1d9b4
Hyeonjeong <ghghjj652@naver.com>
2024-09-22 16:27:56 +0900

    fix: 로그인/회원가입 알림 문구 위치 수정

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -74,92 +74,95 @@ const JoinPage = () => {
         <div className="illustration">
           <img src={loginimage} alt="illustration" />
         </div>
-
-        <form onSubmit={handleSubmit} className="register-form">
-          <div className="title">
-            <h1>회원가입</h1>
-          </div>
-          {error && (
-            <p className="error-message" aria-live="assertive">
-              {error}
-            </p>
-          )}
-          {successMessage && (
-            <p className="success-message" aria-live="assertive">
-              {successMessage}
-            </p>
-          )}{" "}
-          {/* 추가된 성공 메시지 */}
-          <input
-            type="text"
-            name="nickname"
-            value={formData.nickname}
-            onChange={handleChange}
-            placeholder="닉네임"
-            required
-          />
-          <input
-            type="email"
-            name="id"
-            value={formData.id}
-            onChange={handleChange}
-            placeholder="ID (이메일)"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="비밀번호"
-            required
-          />
-          <div className="select-wrap">
-            <select
-              name="developmentField"
-              value={formData.developmentField}
+        <div className="register-form">
+          <form onSubmit={handleSubmit}>
+            <div className="title">
+              <h1>회원가입</h1>
+            </div>
+            <input
+              type="text"
+              name="nickname"
+              value={formData.nickname}
               onChange={handleChange}
+              placeholder="닉네임"
               required
-            >
-              <option value="">개발 분야</option>
-              <option value="프론트엔드 개발자">프론트엔드</option>
-              <option value="백엔드 개발자">백엔드</option>
-              <option value="풀스택 개발자">풀스택</option>
-              <option value="웹 개발자">웹</option>
-              <option value="모바일 개발자">모바일</option>
-              <option value="게임 개발자">게임</option>
-              <option value="DB 개발자">DB</option>
-              <option value="임베디드 SW 개발자">임베디드 SW</option>
-              <option value="보안 개발자">보안</option>
-            </select>
-          </div>
-          <div className="select-wrap">
-            <select
-              name="techStack"
-              value={formData.techStack}
+            />
+            <input
+              type="email"
+              name="id"
+              value={formData.id}
               onChange={handleChange}
+              placeholder="ID (이메일)"
               required
-            >
-              <option value="">기술 스택</option>
-              <option value="React">React</option>
-              <option value="Nodejs">Node.js</option>
-              <option value="Django">Django</option>
-              <option value="C">C</option>
-              <option value="C#">C#</option>
-              <option value="C++">C++</option>
-              <option value="Java">Java</option>
-              <option value="JavaScript">JavaScript</option>
-              <option value="Python">Python</option>
-              <option value="Swift">Swift</option>
-              <option value="R">R</option>
-              <option value="SQL">SQL</option>
-            </select>
-          </div>
-          <button type="submit">회원가입</button>
-          <p>
-            계정이 있으신가요? <a onClick={() => navigate("/login")}>로그인</a>
-          </p>
-        </form>
+            />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="비밀번호"
+              required
+            />
+            <div className="select-wrap">
+              <select
+                name="developmentField"
+                value={formData.developmentField}
+                onChange={handleChange}
+                required
+              >
+                <option value="">개발 분야</option>
+                <option value="프론트엔드 개발자">프론트엔드</option>
+                <option value="백엔드 개발자">백엔드</option>
+                <option value="풀스택 개발자">풀스택</option>
+                <option value="웹 개발자">웹</option>
+                <option value="모바일 개발자">모바일</option>
+                <option value="게임 개발자">게임</option>
+                <option value="DB 개발자">DB</option>
+                <option value="임베디드 SW 개발자">임베디드 SW</option>
+                <option value="보안 개발자">보안</option>
+              </select>
+            </div>
+            <div className="select-wrap">
+              <select
+                name="techStack"
+                value={formData.techStack}
+                onChange={handleChange}
+                required
+              >
+                <option value="">기술 스택</option>
+                <option value="React">React</option>
+                <option value="Nodejs">Node.js</option>
+                <option value="Django">Django</option>
+                <option value="C">C</option>
+                <option value="C#">C#</option>
+                <option value="C++">C++</option>
+                <option value="Java">Java</option>
+                <option value="JavaScript">JavaScript</option>
+                <option value="Python">Python</option>
+                <option value="Swift">Swift</option>
+                <option value="R">R</option>
+                <option value="SQL">SQL</option>
+              </select>
+            </div>
+            {/* 에러 */}
+            {error && !successMessage && (
+              <p className="error message" aria-live="assertive">
+                {error}
+              </p>
+            )}
+            {/* 성공 */}
+            {successMessage && (
+              <p className="success message" aria-live="assertive">
+                {successMessage}
+              </p>
+            )}{" "}
+            <button type="submit">회원가입</button>
+            <p>
+              계정이 있으신가요?{" "}
+              <a onClick={() => navigate("/login")}>로그인</a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import './css/LoginPage.css';
-import { useNavigate } from 'react-router-dom';
-import loginimage from './img/login_logo.svg';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { get, ref } from 'firebase/database';
-import { db, auth } from './firebase/firebase';
+import React, { useState } from "react";
+import "./css/LoginPage.css";
+import { useNavigate } from "react-router-dom";
+import loginimage from "./img/login_logo.svg";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { get, ref } from "firebase/database";
+import { db, auth } from "./firebase/firebase";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
-    id: '',
-    password: ''
+    id: "",
+    password: "",
   });
 
   const navigate = useNavigate();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -26,33 +26,40 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, formData.id, formData.password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        formData.id,
+        formData.password
+      );
       const user = userCredential.user;
 
       if (!user.emailVerified) {
-        setError('이메일 인증을 완료하지 않았습니다. 인증 후 다시 시도해주세요.');
+        setError(
+          "이메일 인증을 완료하지 않았습니다. 인증 후 다시 시도해주세요."
+        );
         return;
       }
 
-      const userRef = ref(db, 'users/' + user.uid);
+      const userRef = ref(db, "users/" + user.uid);
       const snapshot = await get(userRef);
       if (snapshot.exists()) {
         const userData = snapshot.val();
-        console.log('사용자 데이터:', userData);
+        console.log("사용자 데이터:", userData);
       } else {
-        console.log('사용자 데이터가 존재하지 않습니다.');
+        console.log("사용자 데이터가 존재하지 않습니다.");
       }
 
-      navigate('/main');
+      navigate("/main");
     } catch (error) {
-      console.error('로그인 실패:', error);
-      let errorMessage = '로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.';
-      if (error.code === 'auth/user-not-found') {
-        errorMessage = '사용자를 찾을 수 없습니다.';
-      } else if (error.code === 'auth/wrong-password') {
-        errorMessage = '비밀번호가 올바르지 않습니다.';
-      } else if (error.code === 'auth/invalid-email') {
-        errorMessage = '잘못된 이메일 주소입니다.';
+      console.error("로그인 실패:", error);
+      let errorMessage =
+        "로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.";
+      if (error.code === "auth/user-not-found") {
+        errorMessage = "사용자를 찾을 수 없습니다.";
+      } else if (error.code === "auth/wrong-password") {
+        errorMessage = "비밀번호가 올바르지 않습니다.";
+      } else if (error.code === "auth/invalid-email") {
+        errorMessage = "잘못된 이메일 주소입니다.";
       }
       setError(errorMessage);
     }
@@ -69,8 +76,6 @@ const LoginPage = () => {
           <div className="title">
             <h1>로그인</h1>
           </div>
-
-          {error && <p className="error-message" aria-live="assertive">{error}</p>} {/* 로그인 실패 메시지 */}
 
           <input
             type="email"
@@ -89,11 +94,16 @@ const LoginPage = () => {
             placeholder="비밀번호"
             required
           />
-
+          {/* 로그인 실패 메시지 */}
+          {error && (
+            <p className="error message" aria-live="assertive">
+              {error}
+            </p>
+          )}
           <button type="submit">로그인</button>
 
           <p>
-            계정이 없으신가요? <a onClick={() => navigate('/join')}>회원가입</a>
+            계정이 없으신가요? <a onClick={() => navigate("/join")}>회원가입</a>
           </p>
         </form>
       </div>
