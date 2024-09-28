5e462afb601f61edc09b470e43e2407d1f2e7093
Hyeonjeong <ghghjj652@naver.com>
2024-09-28 21:02:13 +0900

    fix: 채팅방 닉네임 나오게 수정

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -2,17 +2,57 @@ import React, { useState, useEffect } from "react";
 import { db } from "../pages/firebase/firebase";
 import { ref, onValue, set } from "firebase/database";
 import { useParams } from "react-router-dom";
-import "./css/ChatRoom.module.css";
+import { getAuth } from "firebase/auth";
+import style from "./css/ChatRoom.module.css";
+import default_profile from "./img/default_profile.svg";
 
 const ChatRoom = () => {
   const { roomId } = useParams();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [nicknames, setNicknames] = useState({});
+  const [userInfos, setUserInfos] = useState({});
+  const [currentUserUid, setCurrentUserUid] = useState("");
 
-  const currentUserUid = "yourCurrentUserUID"; // 현재 로그인한 유저의 uid
+  // 현재 로그인한 유저의 uid 가져오기
+  useEffect(() => {
+    const auth = getAuth();
+    setCurrentUserUid(auth.currentUser?.uid);
+  }, []);
 
   // 채팅 메시지 및 닉네임 불러오기
+  // useEffect(() => {
+  //   const messagesRef = ref(db, `chatrooms/${roomId}/messages`);
+  //   onValue(messagesRef, (snapshot) => {
+  //     const data = snapshot.val();
+  //     if (data) {
+  //       const messagesArray = Object.entries(data).map(([id, message]) => ({
+  //         id,
+  //         ...message,
+  //       }));
+  //       setMessages(messagesArray);
+
+  //       // 메시지에서 각 senderId로 닉네임 조회
+  //       messagesArray.forEach((message) => {
+  //         const { senderId } = message;
+  //         if (!nicknames[senderId]) {
+  //           const userRef = ref(db, `users/${senderId}`);
+  //           onValue(userRef, (userSnapshot) => {
+  //             const userData = userSnapshot.val();
+  //             if (userData && userData.nickname) {
+  //               setNicknames((prevNicknames) => ({
+  //                 ...prevNicknames,
+  //                 [senderId]: userData.nickname,
+  //               }));
+  //             }
+  //           });
+  //         }
+  //       });
+  //     }
+  //   });
+  // }, [roomId, nicknames]);
+
+  // 채팅 메시지 및 유저정보 불러오기
   useEffect(() => {
     const messagesRef = ref(db, `chatrooms/${roomId}/messages`);
     onValue(messagesRef, (snapshot) => {
@@ -27,14 +67,15 @@ const ChatRoom = () => {
         // 메시지에서 각 senderId로 닉네임 조회
         messagesArray.forEach((message) => {
           const { senderId } = message;
-          if (!nicknames[senderId]) {
+          if (!userInfos[senderId]) {
             const userRef = ref(db, `users/${senderId}`);
             onValue(userRef, (userSnapshot) => {
               const userData = userSnapshot.val();
-              if (userData && userData.nickname) {
-                setNicknames((prevNicknames) => ({
-                  ...prevNicknames,
-                  [senderId]: userData.nickname,
+
+              if (userData) {
+                setUserInfos((prevUserInfos) => ({
+                  ...prevUserInfos,
+                  [senderId]: userData,
                 }));
               }
             });
@@ -42,7 +83,7 @@ const ChatRoom = () => {
         });
       }
     });
-  }, [roomId, nicknames]);
+  }, [roomId, userInfos]);
 
   // 메시지 전송 함수
   const handleSendMessage = async () => {
@@ -61,26 +102,30 @@ const ChatRoom = () => {
   };
 
   return (
-    <div className="chatRoom">
-      <div className="messages">
+    <div className={style.chatRoom}>
+      <div className={style.messages}>
         {messages.map((message) => (
-          <div key={message.id} className="message">
-            <span className="sender">
-              {nicknames[message.senderId]}:
-            </span>
+          <div key={message.id} className={style.message}>
+            {/* <span className={style.sender}>{nicknames[message.senderId]}:</span> */}
+            <div className={style.sender}>
+              <img
+                src={userInfos[message.senderId]?.profileImg || default_profile}
+              />
+              {userInfos[message.senderId]?.nickname}:
+            </div>
             {message.text}
           </div>
         ))}
       </div>
-      <div className="inputArea">
+      <div className={style.inputArea}>
         <input
           type="text"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
           placeholder="메시지를 입력하세요..."
-          className="messageInput"
+          className={style.messageInput}
         />
-        <button onClick={handleSendMessage} className="sendButton">
+        <button onClick={handleSendMessage} className={style.sendButton}>
           전송
         </button>
       </div>
