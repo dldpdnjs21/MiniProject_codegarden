f82615f31ad5a0a993cbea98c3d04c59c7179928
Hyeonjeong <ghghjj652@naver.com>
2024-09-29 14:43:23 +0900

    feat: 리뷰 요청 알림 기능 구현

--- src/components/dropdown/Notification.jsx
+++ src/components/dropdown/Notification.jsx
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
 import { db } from "../../pages/firebase/firebase";
 import { ref as dbRef, get } from "firebase/database";
 import line from "../img/line.svg";
+import { useNavigate } from "react-router-dom";
 
 const Notification = ({ useRef }) => {
   const [feedNoti, setFeedNoti] = useState({});
-  const [requestNoti, setRequestNoti] = useState([]);
+  const [requestNoti, setRequestNoti] = useState({});
   const [notiToggle, setNotiToggle] = useState(true);
 
   const fetchNotification = async (uid) => {
@@ -15,11 +16,21 @@ const Notification = ({ useRef }) => {
     const snapshot = await get(notiRef);
     if (snapshot.exists()) {
       const data = snapshot.val();
-      console.log(`Fetched notifications: ${JSON.stringify(data.feedReview)}`);
-      setFeedNoti(data.feedReview);
-      // Object.values(notifications).forEach((notification) => {
-      //   setFeedNoti(notification.contents);
-      // });
+      // console.log(`Fetched notifications: ${JSON.stringify(data.feedReview)}`);
+      // console.log(`Fetched notifications: ${JSON.stringify(data.reviewReq)}`);
+      if (data.feedReview) {
+        // feedReview 객체를 배열로 변환하고 뒤집은 후 다시 객체로 변환
+        const reversedFeedReview = Object.fromEntries(
+          Object.entries(data.feedReview).reverse()
+        );
+        setFeedNoti(reversedFeedReview);
+      }
+      if (data.reviewReq) {
+        const reversedReviewReq = Object.fromEntries(
+          Object.entries(data.reviewReq).reverse()
+        );
+        setRequestNoti(reversedReviewReq);
+      }
     }
   };
 
@@ -37,41 +48,61 @@ const Notification = ({ useRef }) => {
     // 리뷰 요청 알림 가져오기
   }, []);
 
+  const navigate = useNavigate();
+  const handleClickReqNoti = (roomId) => {
+    navigate(`/chatroom/${roomId}`);
+  };
+
   return (
     <div ref={useRef} className={style.notificationDropdown}>
       <div className={style.notiHeader}>알림</div>
-      <div className={style.scrollable}>
-        <div className={style.titleArea}>
-          <div
-            className={`${style.notiTitle} ${notiToggle ? style.active : ""}`}
-            // className={notiToggle ? style.active : ""}
-            onClick={() => setNotiToggle((prev) => !prev)}
-          >
-            리뷰 도착
-          </div>
-          <img src={line} style={{ height: "14px" }} />
-          <div
-            className={`${style.notiTitle} ${notiToggle ? "" : style.active}`}
-            // className={notiToggle ? "" : style.active}
-            onClick={() => setNotiToggle((prev) => !prev)}
-          >
-            리뷰 요청
-          </div>
+      {/* <div className={style.scrollable}> */}
+      <div className={style.titleArea}>
+        <div
+          className={`${style.notiTitle} ${notiToggle ? style.active : ""}`}
+          // className={notiToggle ? style.active : ""}
+          onClick={() => setNotiToggle((prev) => !prev)}
+        >
+          리뷰 도착
         </div>
+        <img src={line} style={{ height: "14px" }} />
+        <div
+          className={`${style.notiTitle} ${notiToggle ? "" : style.active}`}
+          // className={notiToggle ? "" : style.active}
+          onClick={() => setNotiToggle((prev) => !prev)}
+        >
+          리뷰 요청
+        </div>
+      </div>
+      <div className={style.scrollable}>
         {notiToggle ? (
           <ul>
-            {Object.values(feedNoti).map((notification, index) => (
-              <li key={index}>{notification.contents}</li>
-            ))}
+            {Object.keys(feedNoti).length > 0 ? (
+              Object.values(feedNoti).map((notification, index) => (
+                <li key={index}>{notification.contents}</li>
+              ))
+            ) : (
+              <li>알림이 없습니다</li>
+            )}
           </ul>
         ) : (
           <ul>
-            <li>알림 1</li>
-            <li>알림 2</li>
-            <li>알림 3</li>
+            {Object.keys(requestNoti).length > 0 ? (
+              Object.values(requestNoti).map((notification, index) => (
+                <li
+                  key={index}
+                  onClick={() => handleClickReqNoti(notification.chatRoomId)}
+                >
+                  {notification.contents}
+                </li>
+              ))
+            ) : (
+              <li>알림이 없습니다</li>
+            )}
           </ul>
         )}
       </div>
+      {/* </div> */}
     </div>
   );
 };
